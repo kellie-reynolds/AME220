87afd30d5badbbac8edb4c6fb5fb81b95f5d628f
Kellie Reynolds <kmreyno5@asu.edu>
2017-11-13 10:54:36 -0700

    in class

--- DialogLib/dialog.js
+++ DialogLib/dialog.js
@@ -1,11 +1,13 @@
 var dialogLib = {
   alert:{
-    show: function(msg){
+    show: function(msg, label){
 		var addedHTML = "";
+		label = label || "OK";
 		
 		addedHTML = "<div class='wrapper'>" +
 			"<div class='frame'>" +
 				"<h1>" + msg + "</h1>"
+				"<button onClick='dialogLib.alerg.hide()'>" + label + "</button>"+
 			"</div>" +
 			"</div>";
 		
---
+++ add.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+
+app.get("/add", function (req, res) {
+	var a = req.query.a;
+	var b = req.query.b;
+	
+	if(a == undefined || b == undefined) {
+		res.send("Need to define a and b");
+	}
+	else {
+		res.send((parseFloat(a) + parseFloat(b)).toString());
+	}
+});
+
+app.get("/sub", function (req, res) {
+	var a = req.query.a;
+	var b = req.query.b;
+	
+	if(a == undefined || b == undefined) {
+		res.send("Need to define a and b");
+	}
+	else {
+		res.send((parseFloat(a) - parseFloat(b)).toString());
+	}
+});
+
+app.get("/mult", function (req, res) {
+	var a = req.query.a;
+	var b = req.query.b;
+	
+	if(a == undefined || b == undefined) {
+		res.send("Need to define a and b");
+	}
+	else {
+		res.send((parseFloat(a) * parseFloat(b)).toString());
+	}
+});
+
+app.get("/divide", function (req, res) {
+	var a = req.query.a;
+	var b = req.query.b;
+	
+	if(a == undefined || b == undefined) {
+		res.send("Need to define a and b");
+	}
+	else {
+		res.send((parseFloat(a) / parseFloat(b)).toString());
+	}
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
