05b3a80073a175beddfde36dc40fa84a5335bdaf
Kellie Reynolds <kmreyno5@asu.edu>
2017-11-21 20:57:49 -0700

    Assignments

---
+++ Assignment4/dialog.js
@@ -0,0 +1,19 @@
+var data_y;
+
+var dialogLib = {
+  login:{
+    _CB: function(){},
+    _bClicked: function(){
+    },
+    show: function(callback){
+    }
+  },
+
+  signup:{
+    _CB: function(){},
+    _bClicked: function(){
+    },
+    show: function(callback){
+    }
+  }
+}
